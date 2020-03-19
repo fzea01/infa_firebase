1ce1a1f215a7096005c8ea2ec407e2096d15d1ff
fzea01 <nuttahpon_best@hotmail.com>
2020-03-19 11:15:57 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyB9WGJrmVkAv3m8nz_6tPP49BW2czg2Jyo",
+    authDomain: "infa-e6579.firebaseapp.com",
+    databaseURL: "https://infa-e6579.firebaseio.com",
+    projectId: "infa-e6579",
+    storageBucket: "infa-e6579.appspot.com",
+    messagingSenderId: "320068479501",
+    appId: "1:320068479501:web:3a2845ba29d59e0b540344",
+    measurementId: "G-W8T2D2EF2N"
+  };
+
+  export default config;
